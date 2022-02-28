cc68d57bd1d97a5de1322791b36305572d8d0304
Lucas Cristiano Passini <passini@protonmail.com>
2022-02-27 21:48:12 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AddUsers from './components/Users/AddUser';
+
+function App() {
+  return (
+    <div>
+      <AddUsers />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import Button from '../UI/Button';
+import Card from '../UI/Card';
+import classes from './AddUser.module.css';
+
+const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" onChange={usernameChangeHandler} />
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" onChange={ageChangeHandler} />
+        <Button>Add User</Button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
